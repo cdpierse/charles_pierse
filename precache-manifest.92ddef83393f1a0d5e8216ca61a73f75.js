aeb6cbfd154e445cb1a74b9e63f59508034f9ee4
Charles Pierse <charlespierse@gmail.com>
2020-03-07 18:27:50 +0000

    Updates

---
+++ precache-manifest.92ddef83393f1a0d5e8216ca61a73f75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332c5232b8ac5bc9dbff2a9a41782f8a",
+    "url": "/charles_pierse/index.html"
+  },
+  {
+    "revision": "14499020e98f816e7176",
+    "url": "/charles_pierse/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "16dd9d3743886e285fae",
+    "url": "/charles_pierse/static/css/main.3bf7f119.chunk.css"
+  },
+  {
+    "revision": "14499020e98f816e7176",
+    "url": "/charles_pierse/static/js/2.7694e4ce.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/charles_pierse/static/js/2.7694e4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16dd9d3743886e285fae",
+    "url": "/charles_pierse/static/js/main.97cd1dde.chunk.js"
+  },
+  {
+    "revision": "68469b6a3a2d3658491c",
+    "url": "/charles_pierse/static/js/runtime-main.a540f2b4.js"
+  },
+  {
+    "revision": "6ba18710020de22a80a5271880cf509a",
+    "url": "/charles_pierse/static/media/avatar.6ba18710.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charles_pierse/static/media/logo.5d5d9eef.svg"
+  }
+]);
